bee78d1755221a8b4133553642f64dc95d35dc59
jborbely <joe.borbely@gmail.com>
2019-01-04 08:21:24 +1300

    import from collections.abc to fix DeprecationWarning for Py3.8

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -1,4 +1,10 @@
-from collections import MutableMapping, KeysView, ItemsView, ValuesView
+try:
+    # this try..except block fixes:
+    #   DeprecationWarning: Using or importing the ABCs from 'collections' instead
+    #   of from 'collections.abc' is deprecated, and in 3.8 it will stop working
+    from collections.abc import MutableMapping, KeysView, ItemsView, ValuesView
+except ImportError:
+    from collections import MutableMapping, KeysView, ItemsView, ValuesView
 
 import pytest
 
