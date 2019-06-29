0a9c2867b726fdb5cda3ec5b04690539e4e73636
jborbely <joe.borbely@gmail.com>
2019-06-29 13:21:51 +1200

    simplify HDf5Reader.read

--- msl/io/writers/hdf5.py
+++ msl/io/writers/hdf5.py
@@ -9,14 +9,14 @@ Writer for the HDF5_ file format.
 """
 import sys
 
-from .. import Writer
-from ..metadata import Metadata
-from ..base_io import Root
-
 try:
     import h5py
 except ImportError:
     h5py = None
+
+from .. import Writer
+from ..metadata import Metadata
+from ..base_io import Root
 
 if sys.version_info.major > 2:
     unicode = str
@@ -33,14 +33,14 @@ class HDF5Writer(Writer):
             The name of the file to write to. If :data:`None` then uses the value of
             `url` that was specified when :class:`HDF5Writer` was created.
         root : :class:`~msl.io.base_io.Root`, optional
-            Write `root` in HDF5_ format. If :data:`None` the write the
+            Write `root` in HDF5_ format. If :data:`None` then write the
             :class:`~msl.io.group.Group`\\s and :class:`~msl.io.dataset.Dataset`\\s
             in this :class:`HDF5Writer`.
         **kwargs
             All key-value pairs are passed to :class:`File`.
         """
         if h5py is None:
-            raise ImportError('You must install h5py to write HDF5Reader files.\nRun: pip install h5py')
+            raise ImportError('You must install h5py to write HDF5 files.\nRun: pip install h5py')
 
         url = self.url if url is None else url
         if not url:
@@ -51,42 +51,32 @@ class HDF5Writer(Writer):
         elif not isinstance(root, Root):
             raise TypeError('the root parameter must be a Root object')
 
-        def metadata_to_dict(metadata):
-            d = dict()
-            for key, value in metadata.items():
-                d[key] = metadata_to_dict(value) if isinstance(value, Metadata) else value
-            return d
-
         if 'mode' not in kwargs:
             kwargs['mode'] = 'x'  # Create file, fail if exists
 
+        def meta_to_dict(metadata):
+            return dict((k, meta_to_dict(v) if isinstance(v, Metadata) else v)
+                        for k, v in metadata.items())
+
         h5 = h5py.File(url, **kwargs)
-        h5.attrs.update(**metadata_to_dict(root.metadata))
-
-        # HDF5 variable-length string
-        vstr = h5py.special_dtype(vlen=str)
-
+        h5.attrs.update(**meta_to_dict(root.metadata))
         for name, vertex in root.items():
             if self.is_dataset(vertex):
+                data = vertex.data
                 if vertex.dtype.names is not None:
-                    convert = False
-                    dtype = []
+                    convert, dtype = False, []
                     for n in vertex.dtype.names:
                         typ = vertex.dtype.fields[n][0]
-                        if typ == object and isinstance(vertex[n].item(0), (str, unicode)):
-                            dtype.append((n, vstr))
+                        if isinstance(typ, object) and isinstance(vertex[n].item(0), unicode):
+                            # HDF5 variable-length string
+                            dtype.append((n, h5py.special_dtype(vlen=str)))
                             convert = True
                         else:
                             dtype.append((n, typ))
                     if convert:
                         data = vertex.data.astype(dtype=dtype)
-                    else:
-                        data = vertex.data
-                else:
-                    data = vertex.data
                 obj = h5.create_dataset(name, data=data)
             else:
                 obj = h5.create_group(name)
-            obj.attrs.update(**metadata_to_dict(vertex.metadata))
-
+            obj.attrs.update(**meta_to_dict(vertex.metadata))
         h5.close()
