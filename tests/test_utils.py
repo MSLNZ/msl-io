c5ba65b962f5b6cfa5d605d128ffc709fc61ed18
jborbely <joe.borbely@gmail.com>
2019-07-04 09:46:28 +1200

    fix regex pattern for finding __init__.py files not __init__.pyc files

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -13,22 +13,23 @@ def test_search():
     base = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
     assert base.endswith('msl-io')
 
-    files = s(pattern=r'__init__\.py(!?c)')
+    # (?!c) means match __init__.py files but ignore __init__.pyc files
+    files = s(pattern=r'__init__\.py(?!c)')
     assert len(files) == 0
 
-    files = s(pattern=r'__init__\.py(!?c)', levels=1)
+    files = s(pattern=r'__init__\.py(?!c)', levels=1)
     assert len(files) == 1
 
-    files = s(pattern=r'__init__\.py(!?c)', levels=2)
+    files = s(pattern=r'__init__\.py(?!c)', levels=2)
     assert len(files) == 3
 
-    files = s(pattern=r'__init__\.py(!?c)', levels=None)
+    files = s(pattern=r'__init__\.py(?!c)', levels=None)
     assert len(files) == 6
 
-    files = s(pattern=r'__init__\.py(!?c)', levels=None, exclude_folders='readers')
+    files = s(pattern=r'__init__\.py(?!c)', levels=None, exclude_folders='readers')
     assert len(files) == 5
 
-    files = s(pattern=r'__init__\.py(!?c)', levels=None, exclude_folders=['readers', 'writers'])
+    files = s(pattern=r'__init__\.py(?!c)', levels=None, exclude_folders=['readers', 'writers'])
     assert len(files) == 4
 
     files = s(pattern=r'authors')
