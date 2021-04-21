c5115068161705968fe82aaf3d6891cd562ce728
jborbely <joe.borbely@gmail.com>
2021-04-22 11:35:48 +1200

    ExcelReader no longer raises IndexError and check date vs datetime object

--- msl/io/readers/excel.py
+++ msl/io/readers/excel.py
@@ -5,8 +5,10 @@ from datetime import datetime
 
 import xlrd
 
+from .spreadsheet import Spreadsheet
 
-class ExcelReader(object):
+
+class ExcelReader(Spreadsheet):
 
     def __init__(self, file, **kwargs):
         """Read an Excel spreadsheet (.xls and .xlsx).
@@ -28,14 +30,8 @@ class ExcelReader(object):
 
         Examples
         --------
-        >>> from msl.io import ExcelReader
-        >>> excel = ExcelReader('./tests/samples/lab_environment.xlsx')
-        >>> excel.read()
-        [('temperature', 'humidity'), (20.33, 49.82), (20.23, 46.06), (20.41, 47.06), (20.29, 48.32)]
-        >>> excel.read('B2')
-        49.82
-        >>> excel.read('A2:B4')
-        [(20.33, 49.82), (20.23, 46.06), (20.41, 47.06)]
+        >>> from msl.io import ExcelReader  # doctest: +SKIP
+        >>> excel = ExcelReader('lab_environment.xlsx')  # doctest: +SKIP
         """
         # change the default on_demand value
         if 'on_demand' not in kwargs:
@@ -64,33 +60,52 @@ class ExcelReader(object):
         self._workbook.release_resources()
 
     def read(self, cell=None, sheet=None, as_datetime=True):
-        """Read information from the Excel spreadsheet.
+        """Read values from the Excel spreadsheet.
 
         Parameters
         ----------
         cell : :class:`str`, optional
-            The cell(s) to read. For example, ``'C9'`` will return a single value
-            and ``'C9:G20'`` will return all values in the specified range. If not specified
-            then returns all information in the specified `sheet`.
+            The cell(s) to read. For example, ``C9`` will return a single value
+            and ``C9:G20`` will return all values in the specified range. If not
+            specified then returns all values in the specified `sheet`.
         sheet : :class:`str`, optional
-            The name of the sheet to read the information from. If there is only one sheet
-            in the workbook then you do not need to specify the name of the sheet.
+            The name of the sheet to read the value(s) from. If there is only
+            one sheet in the spreadsheet then you do not need to specify the name
+            of the sheet.
         as_datetime : :class:`bool`, optional
-            Whether dates should be returned as :class:`~datetime.datetime` objects.
-            If :data:`False` then dates are returned as an ISO-8601 :class:`str`.
+            Whether dates should be returned as :class:`~datetime.datetime` or
+            :class:`~datetime.date` objects. If :data:`False` then dates are
+            returned as an ISO 8601 string.
 
         Returns
         -------
-        The information in the requested cell(s).
+        The value(s) of the requested cell(s).
+
+        Examples
+        --------
+        .. invisible-code-block: pycon
+
+           >>> from msl.io import ExcelReader
+           >>> excel = ExcelReader('./tests/samples/lab_environment.xlsx')
+
+        >>> excel.read()
+        [('temperature', 'humidity'), (20.33, 49.82), (20.23, 46.06), (20.41, 47.06), (20.29, 48.32)]
+        >>> excel.read('B2')
+        49.82
+        >>> excel.read('A:A')
+        ('temperature', 20.33, 20.23, 20.41, 20.29)
+        >>> excel.read('A1:B1')
+        ('temperature', 'humidity')
+        >>> excel.read('A2:B4')
+        [(20.33, 49.82), (20.23, 46.06), (20.41, 47.06)]
         """
-        if sheet is None:
+        if not sheet:
             names = self._workbook.sheet_names()
             if len(names) > 1:
-                raise IOError('{!r} contains the following sheets:\n  {}\n'
-                              'You must specify the name of the sheet to read'
-                              .format(self._file, ', '.join(names)))
-            else:
-                sheet_name = names[0]
+                raise ValueError('{!r} contains the following sheets:\n  {}\n'
+                                 'You must specify the name of the sheet to read'
+                                 .format(self._file, ', '.join(repr(n) for n in names)))
+            sheet_name = names[0]
         else:
             sheet_name = sheet
 
@@ -104,69 +119,46 @@ class ExcelReader(object):
         if sheet is None:
             raise ValueError('There is no sheet named {!r} in {!r}'.format(sheet_name, self._file))
 
-        if cell is None:
-            start_row, end_row = 0, sheet.nrows - 1
-            start_col, end_col = 0, sheet.ncols - 1
-            return self._get_cell_range(sheet, start_row, end_row, start_col, end_col, as_datetime)
+        if not cell:
+            return [tuple(self._value(sheet, r, c, as_datetime) for c in range(sheet.ncols))
+                    for r in range(sheet.nrows)]
 
-        cells = str(cell).upper().replace('$', '').split(':')
-        if len(cells) == 1:
-            r, c = xlrd.xlsx.cell_name_to_rowx_colx(cells[0])
-            return self._get_cell(sheet, r, c, as_datetime)
-        else:
-            start_row, start_col = xlrd.xlsx.cell_name_to_rowx_colx(cells[0])
-            end_row, end_col = xlrd.xlsx.cell_name_to_rowx_colx(cells[1])
-            return self._get_cell_range(sheet, start_row, end_row, start_col, end_col, as_datetime)
+        split = cell.split(':')
+        r1, c1 = self.to_indices(split[0])
+        if r1 is None:
+            r1 = 0
 
-    def _get_cell_range(self, sheet, start_row, end_row, start_col, end_col, as_datetime):
-        """Get a range of values.
+        if len(split) == 1:
+            try:
+                return self._value(sheet, r1, c1, as_datetime)
+            except IndexError:
+                return
 
-        Parameters
-        ----------
-        sheet : :class:`xlrd.sheet.Sheet`
-            The sheet object.
-        start_row : :class:`int`
-            The starting row (0 based).
-        end_row : :class:`int`
-            The ending row (0 based).
-        start_col : :class:`int`
-            The starting column (0 based).
-        end_col : :class:`int`
-            The ending column (0 based).
-        as_datetime : :class:`bool`
-            Whether dates are :class:`datetime.datetime` or :class:`str`.
+        r2, c2 = self.to_indices(split[1])
+        if r2 is None:
+            r2 = sheet.nrows - 1
 
-        Returns
-        -------
-        :class:`list` or :class:`tuple`
-            The values.
-        """
-        if start_col == end_col:
-            return tuple(self._get_cell(sheet, r, start_col, as_datetime) for r in range(start_row, end_row + 1))
-        elif start_row == end_row:
-            return tuple(self._get_cell(sheet, start_row, c, as_datetime) for c in range(start_col, end_col + 1))
-        else:
-            return [tuple(self._get_cell(sheet, r, c, as_datetime) for c in range(start_col, end_col + 1))
-                    for r in range(start_row, end_row + 1)]
+        if c1 == c2:
+            if c1 >= sheet.ncols:
+                return tuple()
+            return tuple(self._value(sheet, r, c1, as_datetime)
+                         for r in range(r1, min(sheet.nrows, r2+1)))
 
-    def _get_cell(self, sheet, row, col, as_datetime):
-        """Get the value in a cell.
+        if r1 == r2:
+            if r1 >= sheet.nrows:
+                return tuple()
+            return tuple(self._value(sheet, r1, c, as_datetime)
+                         for c in range(c1, min(sheet.ncols, c2+1)))
 
-        Parameters
-        ----------
-        sheet : :class:`xlrd.sheet.Sheet`
-            The sheet object.
-        row : :class:`int`
-            The row number (0 based).
-        col : :class:`int`
-            The column number (0 based).
-        as_datetime : :class:`bool`
-            Whether dates are :class:`datetime.datetime` or :class:`str`.
+        r1 = min(r1, sheet.nrows-1)
+        r2 = min(r2+1, sheet.nrows)
+        c1 = min(c1, sheet.ncols-1)
+        c2 = min(c2+1, sheet.ncols)
+        return [tuple(self._value(sheet, r, c, as_datetime) for c in range(c1, c2))
+                for r in range(r1, r2)]
 
-        Returns
-        -------
-        The value.
-        """
+    def _value(self, sheet, row, col, as_datetime):
+        """Get the value of a cell."""
         cell = sheet.cell(row, col)
         t = cell.ctype
         if t == xlrd.XL_CELL_NUMBER:
@@ -175,10 +167,11 @@ class ExcelReader(object):
             return cell.value
         elif t == xlrd.XL_CELL_DATE:
             dt = datetime(*xlrd.xldate_as_tuple(cell.value, self._workbook.datemode))
+            if dt.hour + dt.minute + dt.second + dt.microsecond == 0:
+                dt = dt.date()
             if as_datetime:
                 return dt
-            else:
-                return dt.isoformat(sep=' ')
+            return str(dt)
         elif t == xlrd.XL_CELL_BOOLEAN:
             return bool(cell.value)
         elif t == xlrd.XL_CELL_EMPTY:
--- tests/test_excel_reader.py
+++ tests/test_excel_reader.py
@@ -1,5 +1,5 @@
 import os
-from datetime import datetime
+from datetime import datetime, date
 
 import pytest
 import xlrd
@@ -16,68 +16,139 @@ def test_raises():
         ExcelReader('does not exist')
 
     # more than one sheet in the Excel workbook
-    with pytest.raises(IOError) as e:
+    with pytest.raises(ValueError, match=r'You must specify the name of the sheet'):
         ExcelReader(file).read()
-    assert 'A1, BH11' in str(e.value)
 
     # the sheet does not exist in the Excel workbook
-    with pytest.raises(ValueError) as e:
+    with pytest.raises(ValueError, match=r'There is no sheet named'):
         ExcelReader(file).read(sheet='XXXYYYZZZ')
-    assert 'XXXYYYZZZ' in str(e.value)
-
-    # the cell range is invalid
-    with pytest.raises(Exception) as e:
-        ExcelReader(file).read(cell='A-B', sheet='A1')
-    assert 'Unexpected character' in str(e.value)
-
-    with pytest.raises(IndexError):
-        ExcelReader(file).read(cell='AH42', sheet='A1')
 
 
-def test_read():
-    # NOTE: Reading a range of cells is tested in test_read_table.py
-
+def test_cell():
     file = os.path.join(os.path.dirname(__file__), 'samples', 'table.xlsx')
-    excel = ExcelReader(file)
+    excel = ExcelReader(file, on_demand=True)
+    values = [
+        ('timestamp', 'val1', 'uncert1', 'val2', 'uncert2'),
+        (datetime(2019, 9, 11, 14, 6, 55), -0.505382, 0.000077, 0.501073, 0.000079),
+        (datetime(2019, 9, 11, 14, 6, 59), -0.505191, 0.000066, 0.500877, 0.000083),
+        (datetime(2019, 9, 11, 14, 7, 3), -0.505308, 0.000086, 0.500988, 0.000087),
+        (datetime(2019, 9, 11, 14, 7, 7), -0.505250, 0.000119, 0.500923, 0.000120),
+        (datetime(2019, 9, 11, 14, 7, 11), -0.505275, 0.000070, 0.500965, 0.000088),
+        (datetime(2019, 9, 11, 14, 7, 15), -0.505137, 0.000079, 0.500817, 0.000085),
+        (datetime(2019, 9, 11, 14, 7, 19), -0.505073, 0.000099, 0.500786, 0.000084),
+        (datetime(2019, 9, 11, 14, 7, 23), -0.505133, 0.000088, 0.500805, 0.000076),
+        (datetime(2019, 9, 11, 14, 7, 27), -0.505096, 0.000062, 0.500759, 0.000062),
+        (datetime(2019, 9, 11, 14, 7, 31), -0.505072, 0.000142, 0.500739, 0.000149)
+    ]
+
     assert excel.file == file
     assert isinstance(excel.workbook, xlrd.book.Book)
+    sheet = excel.workbook.sheet_by_name('A1')
+    assert sheet.nrows == 11
+    assert sheet.ncols == 5
+    sheet = excel.workbook.sheet_by_name('BH11')
+    assert sheet.nrows == 21
+    assert sheet.ncols == 64
+    sheet = excel.workbook.sheet_by_name('AEX154041')
+    assert sheet.nrows == 154051
+    assert sheet.ncols == 834
+
+    # single cell
     assert excel.read(cell='A1', sheet='A1') == 'timestamp'
-    assert excel.read(cell='A7', sheet='A1') == datetime(2019, 9, 11, 14, 7, 15)
-    assert excel.read(cell='A7', sheet='A1', as_datetime=False) == '2019-09-11 14:07:15'
     assert excel.read(cell='B2', sheet='A1') == -0.505382
+    assert excel.read(cell='C7', sheet='A1') == 0.000079
+    assert excel.read(cell='A100', sheet='A1') is None  # A100 is empty (also out of bounds)
+    assert excel.read(cell='BI12', sheet='BH11') == -0.505382
+    assert excel.read(cell='A1', sheet='BH11') is None  # A1 is empty
+    assert excel.read(cell='ZZ1000', sheet='BH11') is None  # ZZ1000 is empty (also out of bounds)
     assert excel.read(cell='AFB154045', sheet='AEX154041') == 0.00012
-    assert excel.read(cell='A1', sheet='BH11') is None
+    assert excel.read(cell='AA25', sheet='AEX154041') is None  # AA25 is empty
+    assert excel.read(cell='BAA200000', sheet='AEX154041') is None  # BAA200000 is empty (also out of bounds)
 
-    # check that calling close() multiple times is okay (on_demand is True by default)
-    excel.close()
-    excel.close()
-    excel.close()
-    excel.close()
-    excel.close()
-    excel.close()
-    excel.close()
-    excel.close()
+    # single row
+    assert excel.read(cell='A1:E1', sheet='A1') == values[0]
+    assert excel.read(cell='BH11:BL11', sheet='BH11') == values[0]
+    assert excel.read(cell='A2:E2', sheet='A1') == values[1]
+    assert excel.read(cell='BH12:BL12', sheet='BH11') == values[1]
+    assert excel.read(cell='A3:E3', sheet='A1') == values[2]
+    assert excel.read(cell='BH13:BL13', sheet='BH11') == values[2]
+    assert excel.read(cell='A4:E4', sheet='A1') == values[3]
+    assert excel.read(cell='BH14:BL14', sheet='BH11') == values[3]
+    assert excel.read(cell='A5:E5', sheet='A1') == values[4]
+    assert excel.read(cell='BH15:BL15', sheet='BH11') == values[4]
+    assert excel.read(cell='A6:E6', sheet='A1') == values[5]
+    assert excel.read(cell='BH16:BL16', sheet='BH11') == values[5]
+    assert excel.read(cell='A7:E7', sheet='A1') == values[6]
+    assert excel.read(cell='BH17:BL17', sheet='BH11') == values[6]
+    assert excel.read(cell='A8:E8', sheet='A1') == values[7]
+    assert excel.read(cell='BH18:BL18', sheet='BH11') == values[7]
+    assert excel.read(cell='A9:E9', sheet='A1') == values[8]
+    assert excel.read(cell='BH19:BL19', sheet='BH11') == values[8]
+    assert excel.read(cell='A10:E10', sheet='A1') == values[9]
+    assert excel.read(cell='BH20:BL20', sheet='BH11') == values[9]
+    assert excel.read(cell='A11:E11', sheet='A1') == values[10]
+    assert excel.read(cell='BH21:BL21', sheet='BH11') == values[10]
+    assert excel.read(cell='A6:B6', sheet='A1') == values[5][:2]
+    assert excel.read(cell='A12:C12', sheet='A1') == ()  # row 12 is empty (also out of bounds)
+    assert excel.read(cell='A1000:Z1000', sheet='A1') == ()  # row 1000 is empty (also out of bounds)
+    assert excel.read(cell='BH22:BL22', sheet='BH11') == ()  # row 22 is empty (also out of bounds)
+    assert excel.read(cell='A1000:ZZ1000', sheet='BH11') == ()  # row 1000 is empty (also out of bounds)
+    assert excel.read(cell='A1:A1', sheet='A1') == ('timestamp',)
+    assert excel.read(cell='D9:D9', sheet='A1') == (0.500805,)
 
+    # single column
+    assert excel.read(cell='A:A', sheet='A1') == tuple(item[0] for item in values)
+    assert excel.read(cell='B:B', sheet='A1') == tuple(item[1] for item in values)
+    assert excel.read(cell='C:C', sheet='A1') == tuple(item[2] for item in values)
+    assert excel.read(cell='D:D', sheet='A1') == tuple(item[3] for item in values)
+    assert excel.read(cell='E:E', sheet='A1') == tuple(item[4] for item in values)
+    assert excel.read(cell='F:F', sheet='A1') == ()  # column F is empty (also out of bounds)
+    assert excel.read(cell='ABC:ABC', sheet='A1') == ()  # column ABC is empty (also out of bounds)
+    assert excel.read(cell='BH:BH', sheet='BH11') == tuple(None for _ in range(10)) + tuple(item[0] for item in values)
+    assert excel.read(cell='A:A', sheet='BH11') == tuple(None for _ in range(21))  # column A is empty
+    assert excel.read(cell='BG:BG', sheet='BH11') == tuple(None for _ in range(21))  # column BG is empty
+
+    # 2D slices
+    assert excel.read(cell='A:E', sheet='A1') == values
+    assert excel.read(cell='A1:E11', sheet='A1') == values
+    assert excel.read(cell='A1:AAA10000', sheet='A1') == values  # slicing out of range is okay
+    assert excel.read(cell='A1:C6', sheet='A1') == [row[:3] for row in values[:6]]
+    assert excel.read(cell='A10:E11', sheet='A1') == values[-2:]
+    assert excel.read(cell='A10:E1000', sheet='A1') == values[-2:]  # slicing out of range is okay
+    assert excel.read(cell='A:E', sheet='BH11') == [tuple(None for _ in range(5)) for _ in range(21)]
+    new = [tuple(None for _ in range(6))]
+    for row in values:
+        new.append((None,) + row)
+    assert excel.read(cell='BG10:BM22', sheet='BH11') == new
+    assert excel.read(cell='BK20:BL21', sheet='BH11') == [row[-2:] for row in values[-2:]]
+    assert excel.read(cell='AEX154041:AFB154051', sheet='AEX154041') == values
+    assert excel.read(cell='AEX154041:ZZZ1000000', sheet='AEX154041') == values  # slicing out of range is okay
+    assert excel.read(cell='AEY154042:AFA154044', sheet='AEX154041') == [row[1:4] for row in values[1:4]]
+
+    # calling close() multiple times is okay (on_demand is True by default)
+    for _ in range(10):
+        excel.close()
+
+
+def test_datatypes():
     # the following workbook only contains 1 sheet so we don't have to specify the sheet
     file = os.path.join(os.path.dirname(__file__), 'samples', 'excel_datatypes.xlsx')
     excel = ExcelReader(file, on_demand=False)
     assert excel.file == file
     assert excel.workbook.nsheets == 1
     assert excel.workbook.sheet_names()[0] == 'Sheet1'
-    assert excel.read(cell='A1') == 1.23  # '$1.23' -> currency is just a number
+    assert excel.read(cell='A1') == 1.23  # '$1.23'
     assert excel.read(cell='B1') is True
     assert excel.read(cell='C1') == datetime(2019, 9, 13, 13, 20, 22)
     assert excel.read(cell='C1', as_datetime=False) == '2019-09-13 13:20:22'
+    assert excel.read(cell='D1') == date(2019, 9, 13)
+    assert excel.read(cell='D1', as_datetime=False) == '2019-09-13'
     assert excel.read(cell='A2') == 3.141592653589793
     assert excel.read(cell='B2') == 'some text'
     assert excel.read(cell='C2') is None
+    assert excel.read(cell='D2') == 0.34  # '34%'
     assert excel.read(cell='A1:A2') == (1.23, 3.141592653589793)
 
-    # check that calling close() multiple times is okay when on_demand=False
-    excel.close()
-    excel.close()
-    excel.close()
-    excel.close()
-    excel.close()
-    excel.close()
-    excel.close()
+    # calling close() multiple times is okay when on_demand=False
+    for _ in range(10):
+        excel.close()
