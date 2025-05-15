c99d080edf7e5d1a0ccad2e4fd7dc2b0627a9d77
jborbely <joe.borbely@gmail.com>
2025-05-15 12:59:35 +1200

    switch to src layout and pyproject.toml

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -35,27 +35,30 @@ def test_search():
     assert len(files) == 0
 
     files = s(pattern=r'__init__\.py(?!c)', levels=1)
-    assert len(files) == 1
-
-    files = s(pattern=r'__init__\.py(?!c)', levels=2)
-    assert len(files) == 3
-
-    files = s(pattern=r'__init__\.py(?!c)', levels=None, exclude_folders=r'v?env')
-    assert len(files) == 7
-
-    files = s(pattern=r'__init__\.py(?!c)', levels=None, exclude_folders=['readers', 'v?env'])
-    assert len(files) == 5
-
-    files = s(pattern=r'__init__\.py(?!c)', levels=None, exclude_folders=['readers', 'writers', 'v?env'])
-    assert len(files) == 4
-
-    files = s(pattern=r'authors')
     assert len(files) == 0
 
-    files = s(pattern=r'authors', regex_flags=re.IGNORECASE)
+    files = s(pattern=r'__init__\.py(?!c)', levels=2)
+    assert len(files) == 0
+
+    files = s(pattern=r'__init__\.py(?!c)', levels=3)
     assert len(files) == 1
 
-    files = s(pattern=r'setup')
+    files = s(pattern=r'__init__\.py(?!c)', levels=None, exclude_folders=r'v?env')
+    assert len(files) == 4
+
+    files = s(pattern=r'__init__\.py(?!c)', levels=None, exclude_folders=['readers', 'v?env'])
+    assert len(files) == 2
+
+    files = s(pattern=r'__init__\.py(?!c)', levels=None, exclude_folders=['readers', 'writers', 'v?env'])
+    assert len(files) == 1
+
+    files = s(pattern=r'license')
+    assert len(files) == 0
+
+    files = s(pattern=r'license', regex_flags=re.IGNORECASE)
+    assert len(files) == 1
+
+    files = s(pattern=r'\.(toml|json)')
     assert len(files) == 2
 
     files = s(pattern=r'README', levels=None, exclude_folders='v?env')
@@ -84,7 +87,7 @@ def test_git_head():
 
     # can specify any directory within the version control hierarchy
     assert utils.git_head(os.path.join(root_dir, '.git')) == head
-    assert utils.git_head(os.path.join(root_dir, 'msl', 'examples', 'io')) == head
+    assert utils.git_head(os.path.join(root_dir, 'src', 'msl', 'io', 'readers')) == head
     assert utils.git_head(os.path.dirname(__file__)) == head
     assert utils.git_head(os.path.join(root_dir, 'docs', '_api')) == head
 
